1e3cca746620db9d38b9e12a826fa86c1a098f40
Kevin Trotter <cskevint@gmail.com>
2012-06-05 11:35:30 -0700

    graph updates and intellij lib config updates

--- src/graph.js
+++ src/graph.js
@@ -20,12 +20,15 @@ Ext.ns("C3.PEAT.ux.Graph");
 
         } else if(Button.daily.pressed) {
             Graph.panel.items.add(current);
-            window.alert("not implemented yet");
+            console.log("not implemented yet");
         }
         Graph.panel.doLayout();
     };
 
     Button.annual = new Ext.button.Button({
+        cls: "graphTab",
+        overCls: "graphTabOver",
+        pressedCls: "graphTabPressed",
         text: "Annual",
         toggleGroup: "dateType",
         pressed: true,
@@ -33,49 +36,71 @@ Ext.ns("C3.PEAT.ux.Graph");
     });
 
     Button.monthly = new Ext.button.Button({
+        cls: "graphTab",
+        overCls: "graphTabOver",
+        pressedCls: "graphTabPressed",
         text: "Monthly",
         toggleGroup: "dateType",
         handler: dateTypeHandler
     });
 
     Button.daily = new Ext.button.Button({
+        cls: "graphTab",
+        overCls: "graphTabOver",
+        pressedCls: "graphTabPressed",
         text: "Daily",
         toggleGroup: "dateType",
         handler: dateTypeHandler
     });
 
     Button.total = new Ext.button.Button({
+        cls: "graphTab",
+        overCls: "graphTabOver",
+        pressedCls: "graphTabPressed",
         text: "Total",
         pressed: true,
         toggleGroup: "data",
         handler: function(){
-            Chart.monthly.series.items[1].hideAll();
-            Chart.monthly.series.items[2].hideAll();
-            Chart.monthly.series.items[0].showAll();
+            Chart.monthly.series.items[0].toggleAll(true);
+            Chart.monthly.series.items[1].toggleAll(false)
+            Chart.monthly.series.items[2].toggleAll(false)
+//            Chart.monthly.series.items[1].hideAll(0);
+//            Chart.monthly.series.items[2].hideAll(0);
+//            Chart.monthly.series.items[0].showAll(0);
+
         }
     });
 
     Button.electricity = new Ext.button.Button({
+        cls: "graphTab",
+        overCls: "graphTabOver",
+        pressedCls: "graphTabPressed",
         text: "Electricity",
         toggleGroup: "data",
         handler: function(){
-            Chart.monthly.series.items[0].hideAll();
-            Chart.monthly.series.items[2].hideAll();
-            Chart.monthly.series.items[1].showAll();
+            Chart.monthly.series.items[0].hideAll(0);
+            Chart.monthly.series.items[2].hideAll(0);
+            Chart.monthly.series.items[1].showAll(0);
         }
     });
 
     Button.gas = new Ext.button.Button({
+        cls: "graphTab",
+        overCls: "graphTabOver",
+        pressedCls: "graphTabPressed",
         text: "Gas",
         toggleGroup: "data",
         handler: function(){
-            Chart.monthly.series.items[0].hideAll();
-            Chart.monthly.series.items[1].hideAll();
-            Chart.monthly.series.items[2].showAll();
+            Chart.monthly.series.items[0].hideAll(0);
+            Chart.monthly.series.items[1].hideAll(0);
+            Chart.monthly.series.items[2].showAll(0);
         }
     });
 
     Button.byUse = new Ext.button.Button({
+        cls: "graphTab",
+        overCls: "graphTabOver",
+        pressedCls: "graphTabPressed",
         text: "By Use",
         toggleGroup: "data",
         handler: function(){
@@ -84,6 +109,9 @@ Ext.ns("C3.PEAT.ux.Graph");
     });
 
     Button.previous = new Ext.button.Button({
+        cls: "graphTab",
+        overCls: "graphTabOver",
+        pressedCls: "graphTabPressed",
         text: "Previous Period",
         enableToggle: true,
         handler: function(){
@@ -92,6 +120,9 @@ Ext.ns("C3.PEAT.ux.Graph");
     });
 
     Button.actions = new Ext.button.Button({
+        cls: "graphTab",
+        overCls: "graphTabOver",
+        pressedCls: "graphTabPressed",
         text: "Plan Actions",
         enableToggle: true,
         handler: function(){
@@ -100,6 +131,9 @@ Ext.ns("C3.PEAT.ux.Graph");
     });
 
     Button.weather = new Ext.button.Button({
+        cls: "graphTab",
+        overCls: "graphTabOver",
+        pressedCls: "graphTabPressed",
         text: "Weather",
         enableToggle: true,
         handler: function(){
@@ -114,7 +148,7 @@ Ext.ns("C3.PEAT.ux.Graph");
     Data.annual = new Ext.data.JsonStore({
         fields: ["name", "data"],
         data: [
-//            {name: "Previous Year", data: 10},
+            {name: "Previous Year", data: 10},
             {name: "Current Year", data: 20},
             {name: "Efficient", data: 10},
             {name: "Average", data: 50}
@@ -125,7 +159,7 @@ Ext.ns("C3.PEAT.ux.Graph");
         style: "background:#fff",
 //        animate: true,
         store: Data.annual,
-        shadow: true,
+        shadow: false,
         axes: [{
             type: "Numeric",
             minimum: 0,
@@ -135,13 +169,13 @@ Ext.ns("C3.PEAT.ux.Graph");
             dashSize: 0,
             title: "Dollars",
             grid: true
-        },{
+        }/*,{
             type: "Category",
             position: "bottom",
             fields: ["name"]
-        }],
+        }*/],
         series: [{
-            type: "column",
+            type: "annualbarchart",
             axis: "left",
             xField: "name",
             yField: "data",
@@ -152,8 +186,8 @@ Ext.ns("C3.PEAT.ux.Graph");
                 renderer: Ext.util.Format.numberRenderer('$0'),
                 orientation: 'horizontal',
                 color: '#333'
-            },
-            renderer: function(sprite, storeItem, barAttr, i, store) {
+            }
+            ,renderer: function(sprite, storeItem, barAttr, i, store) {
                 if(store.data.items.length > 3) {
                     switch(i) {
                         case 0:
@@ -181,8 +215,8 @@ Ext.ns("C3.PEAT.ux.Graph");
                             barAttr.fill = "gray";
                     }
                 }
-                barAttr.x += parseInt(barAttr.width)/2-25;
-                barAttr.width = "50px";
+//                barAttr.x += parseInt(barAttr.width)/2-25;
+//                barAttr.width = "50px";
                 return barAttr;
             }
         }]
@@ -293,6 +327,7 @@ Ext.ns("C3.PEAT.ux.Graph");
 //        animate: true,
         store: Data.monthly,
         shadow: true,
+        legend: true,
         axes: [{
             type: "Numeric",
             minimum: 0,
@@ -313,8 +348,8 @@ Ext.ns("C3.PEAT.ux.Graph");
     Chart.monthly.on({
         render: function(){
             setTimeout(function(){
-                Chart.monthly.series.items[1].hideAll();
-                Chart.monthly.series.items[2].hideAll();
+//                Chart.monthly.series.items[1].hideAll();
+//                Chart.monthly.series.items[2].hideAll();
                 //Chart.monthly.series.items[0].showAll();
             }, 0);
         }
@@ -348,3 +383,150 @@ Ext.ns("C3.PEAT.ux.Graph");
     });
 
 })(C3.PEAT.ux.Graph);
+
+Ext.define('C3.ui.widget.chart.series.VariableWidthColumn', {
+
+    extend: 'Ext.chart.series.Bar',
+
+    type: 'annualbarchart',
+
+    alias: ['series.annualbarchart'],
+
+    alternateClassName: [],
+
+    column: true,
+
+    /**
+     * @cfg style Style properties that will override the theming series styles.
+     */
+    style: {
+        color : '#0000FF',
+        fill : '#0000FF'
+    },
+
+    /**
+     * @cfg {Number} gutter The gutter space between single bars, as a percentage of the bar width
+     */
+    gutter: 0,
+
+    /**
+     * @cfg {Number} groupGutter The gutter space between groups of bars, as a percentage of the bar width
+     */
+    groupGutter: 0,
+
+    /**
+     * @cfg {Number} xPadding Padding between the left/right axes and the bars
+     */
+    xPadding: 0,
+
+    /**
+     * @cfg {Number} yPadding Padding between the top/bottom axes and the bars
+     */
+    yPadding: 0,
+
+    // @private Build an array of paths for the chart
+    getPaths: function() {
+        var me = this,
+            chart = me.chart,
+
+            store = chart.getChartStore(),
+
+            data = store.data.items,
+
+            i, total, record,
+
+            bounds = me.bounds = me.getBounds(),
+
+            items = me.items = [],
+
+            yFields = me.yField,
+
+            gutter = me.gutter / 100,
+
+            groupGutter = me.groupGutter / 100,
+
+            bbox = bounds.bbox,
+
+            barWidth = bounds.barWidth,
+
+            shrunkBarWidth = bounds.shrunkBarWidth,
+
+            xPadding = me.xPadding,
+
+            barsLen = bounds.barsLen,
+
+            colors = me.colorArrayStyle,
+
+            colorLength = colors && colors.length || 0,
+
+            j, yValue, height, bottom, top, hasShadow, barAttr, counter, floorY;
+
+
+        var hasPrevious = data.length > 3;
+
+        for (i = 0, total = data.length; i < total; i++) {
+            record = data[i];
+            bottom = bounds.zero;
+            top = bounds.zero;
+            hasShadow = false;
+
+            for (j = 0, counter = 0; j < barsLen; j++) {
+
+                yValue = record.get(bounds.bars[j]);
+
+                height = Math.round((yValue - Math.max(bounds.minY, 0)) * bounds.scale);
+
+                barAttr = {
+                    fill: colors[(barsLen > 1 ? j : 0) % colorLength]
+                };
+
+                var padding = 50;
+                if(i == 0 && hasPrevious) {
+                    padding = 70;
+                }
+
+                var xCoord = 20 + ((i == 0 && hasPrevious)? 60 : 0)
+                        + bbox.x
+                        + xPadding
+                        + (barWidth - shrunkBarWidth) * 0.5
+                        + i * barWidth * (1 + gutter)
+                        + counter * bounds.groupBarWidth * (1 + groupGutter);
+
+                Ext.apply(barAttr, {
+                    height: height,
+                    width: Math.max(bounds.groupBarWidth, 0) - padding,
+                    x: xCoord,
+                    y: bottom - height
+                });
+
+                if (height < 0) {
+                    barAttr.y = top;
+                    barAttr.height = Math.abs(height);
+                }
+
+                barAttr.x = Math.floor(barAttr.x) + 1;
+
+                floorY = Math.floor(barAttr.y);
+                if (!Ext.isIE9 && barAttr.y > floorY) {
+                    floorY--;
+                }
+                barAttr.y = floorY;
+
+                barAttr.width = Math.floor(barAttr.width);
+
+                barAttr.height = Math.floor(barAttr.height);
+
+                items.push({
+                    series: me,
+                    yField: yFields[j],
+                    storeItem: record,
+                    value: [record.get(me.xField), yValue],
+                    attr: barAttr,
+                    point: [barAttr.x + barAttr.width / 2, yValue >= 0 ? barAttr.y : barAttr.y + barAttr.height]
+                });
+
+                counter++;
+            }
+        }
+    }
+});
